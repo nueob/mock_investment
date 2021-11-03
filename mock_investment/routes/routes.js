0d9db5bcc71c6fa84de0fe681360c19746d60510
nueob <79954748+nueob@users.noreply.github.com>
2021-11-03 21:14:08 +0900

    mvc 패턴 적용

---
+++ mock_investment/routes/routes.js
@@ -0,0 +1,8 @@
+// var express = require('express');
+// var router = express.Router();
+// var userController = require('../controllers/UserController');
+
+// router.get('/users',userController.doGetUser);
+// router.get('/users/regist',userController.doRegistUser);
+
+// module.exports = router;
