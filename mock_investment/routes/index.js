1b19e03c4827a9864427e48a3e5ec53e91548ba2
nueob <79954748+nueob@users.noreply.github.com>
2021-11-03 21:05:08 +0900

    view 경로 설정 및 페이지 나누기

--- mock_investment/routes/index.js
+++ mock_investment/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('auth-login', { title: 'Express' });
+  res.render('user-auth/auth-login', { title: 'Express' });
 });
 
 module.exports = router;
