b100e714c2f1e6402def1ebb36320a81420d84d6
nueob <79954748+nueob@users.noreply.github.com>
2021-11-04 19:33:55 +0900

    page layout 적용
    header, footer 적용

--- mock_investment/app.js
+++ mock_investment/app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+var expressLayouts = require('express-ejs-layouts');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,12 +9,16 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mainRouter = require('./routes/main');
 var searchRouter = require('./routes/stock-search');
+const expressEjsLayouts = require('express-ejs-layouts');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout','layout');
+app.set('layout extractScriptes',true);
+app.use(expressLayouts);
 
 app.use(logger('dev'));
 app.use(express.json());
