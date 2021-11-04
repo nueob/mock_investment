2b335c8c67956155b98a40b288a23e265863d682
nueob <79954748+nueob@users.noreply.github.com>
2021-11-04 21:26:38 +0900

    종목 검색 페이지 > 검색, 탭
    종목 검색 페이지 > 검색, 탭 추가
    - bootstrap 4

--- mock_investment/routes/stock-search.js
+++ mock_investment/routes/stock-search.js
@@ -7,4 +7,12 @@ router.get('/', function(req, res, next) {
   res.render('sample/index2', { title: 'Express' });
 });
 
+router.get('/sample', function(req, res, next) {
+  res.render('sample/ui-widgets-todolist', { title: 'Express' });
+});
+
+router.get('/stock', function(req, res, next) {
+  res.render('stock-search/stock-search', { title: 'Express' });
+});
+
 module.exports = router;
