85a02ed8fd32987dd2b767096ef2f9c6c7ba6d74
nahyeonung <ntweety@naver.com>
2021-11-04 23:50:14 +0900

    main화면 수정

--- mock_investment/routes/main.js
+++ mock_investment/routes/main.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('sample/index', { title: 'Express' });
+  res.render('dashboard/index', { title: 'Express' });
 });
 
 module.exports = router;
