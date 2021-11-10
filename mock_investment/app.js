9fc17a160950c2341b1adc96f3b79b92a9b6f915
nahyeonung <ntweety@naver.com>
2021-11-11 02:48:08 +0900

    session방식으로 로그인

--- mock_investment/app.js
+++ mock_investment/app.js
@@ -5,14 +5,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var session = require('express-session');
+//var bodyParser = require('body-parser');
+
+//var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mainRouter = require('./routes/main');
 var searchRouter = require('./routes/stock-search');
 const expressEjsLayouts = require('express-ejs-layouts');
 
 var app = express();
-
+//app.use(bodyParser.urlencoded({ extended: false}));
+app.use(session({
+  secret: 'sdfdsf3',
+  resave: false,
+  saveUninitialized: true
+}));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -26,12 +34,41 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/main', mainRouter);
 app.use('/search', searchRouter);
 app.use('/users', usersRouter);
 
+app.get('/', (req,res)=>{
+  res.render("user-auth/auth-login", {title: 'Express'});
+})
 
+app.post('/', (req,res)=>{
+  var user = {
+    user_name: '한남대',
+    user_password: '1111'
+  };
+  var um = req.body.um;
+  var pwd = req.body.pwd;
+    
+  if( um ===  user.user_name && pwd === user.user_password){
+    req.session.displayname = req.body.displayname;  //이거 좌,우 순서만 바껴도 틀린다........
+    res.render('dashboard/index', { title: 'Express' });
+  }else{
+    res.send(um+','+pwd+'은 잘못된 아이디와 로그인입니다. <a href="/">login</a>');
+  }
+});
+
+app.get('/register', (req,res)=>{
+  res.render("user-auth/auth-register");
+});
+// app.get('/', function(req,res){
+//   req.session.count=1;
+//   res.send('ge');
+// });
+app.listen('3000', function(){
+  console.log('success');
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- mock_investment/routes/main.js
+++ mock_investment/routes/main.js
@@ -1,12 +1,29 @@
 // 메인 창
 var express = require('express');
+var session = require('express-session');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('dashboard/index', { title: 'Express' });
+router.post('/', function(req, res, next){
+  if(req.session.displayname){
+    res.render("dashboard/index");
+  } else{
+    res.send("please login first");
+  }
+  
 });
 module.exports = router;
+
+/* session을 받을 수 있는 코딩 */
+// router.post('/', function(req, res, next) {
+//   if(req.session.count){
+//     req.session.count++;
+//   } else{
+//     req.session.count = 1;
+//   }
+//   //res.render('dashboard/index', { title: 'Express' });
+//   res.send('fianl : '+req.session.count);
+// });
+// module.exports = router;
 
 // router.post('/', function(req, res, next){
 //   var id = req.body.id;
@@ -21,17 +38,3 @@ module.exports = router;
 //   }
 
 // });
-
-// router.post('/', function(req, res, next){
-//     var id = req.body.id;
-//     var password = req.body.password;
-//     var um = req.body.um;
-//     var pwd = req.body.pwd;
-//     if(req.body.um ===  "ntweety"){
-//       res.render('dashboard/index', { title: 'Express' });
-//     }else{
-//       res.send(um+','+pwd);
-//     }
-  
-//   });
-//   module.exports = router;
