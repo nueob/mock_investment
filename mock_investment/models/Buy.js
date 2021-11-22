cfba5313dde3e60e8a1af9b05f27517c595f4913
nahyeonung <ntweety@naver.com>
2021-11-22 22:36:11 +0900

    매수가와 현재가 연결(미완)

--- mock_investment/models/Buy.js
+++ mock_investment/models/Buy.js
@@ -7,15 +7,24 @@ const table2 = 'stock_sell_item';
 const table3 = 'company_info';
 
 module.exports = {
-    buyStock : function(stock, user_idx, company_Idx, stock_count) {
+    buyStock : function(stock, user_idx, company_Idx) {
         return new Promise ((resolve,reject)=> {
-            dbconn.query( `insert into ${table} (get_buy_stock, user_idx, company_Idx) values ('${stock}','${user_idx}','${company_Idx}');`, (err,result,fields) =>
+            var query = `insert into ${table} (get_buy_stock, user_idx, company_Idx) values ('${stock}','${user_idx}','${company_Idx}');` +
+                        `select count(*) as cnt from stock_buy_item b
+                        inner join company_info c on b.company_idx = c.company_idx
+                        where b.get_buy_stock = c.company_stock `
+            dbconn.query( query, (err,result,fields) =>
                 {
                     if(err){
-                        console.log(err);
                         reject(err);
                     } else {
-                        resolve(result);
+                        let res = JSON.parse(JSON.stringify(result));
+                        
+                        if(res[1][0].cnt > 5) {
+                            this.updateGetMoney(company_Idx);
+                        } else {
+                            resolve(res[1][0].cnt);
+                        }
                     }
                 }
             )
@@ -26,6 +35,22 @@ module.exports = {
         return new Promise ((resolve, reject)=>{
             dbconn.query(
                 `insert into ${table2} (get_sell_stock, user_idx, company_Idx) values ('${stock}','${user_idx}','${company_Idx}')`, (err,result,fields) =>
+                {
+                    if(err){
+                        console.log(err);
+                        reject(err);
+                    } else{
+                        resolve(result);
+                    }
+                }
+            )
+        })
+    },
+
+    updateGetMoney : function(company_Idx){
+        return new Promise ((resolve, reject) =>{
+            dbcoon.query(
+                `update company_info set company_stock = concat(company_stock + 500) where company_idx = '${company_Idx}';`, (err,result,fields) =>
                 {
                     if(err){
                         console.log(err);
