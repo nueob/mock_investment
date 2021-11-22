0e00156bf4db32178f94b3f71895a49a27c3ab4e
nahyeonung <ntweety@naver.com>
2021-11-23 00:09:11 +0900

    매수 매도 주식값 연동

--- mock_investment/models/Buy.js
+++ mock_investment/models/Buy.js
@@ -19,11 +19,46 @@ module.exports = {
                         reject(err);
                     } else {
                         let res = JSON.parse(JSON.stringify(result));
-                        
-                        if(res[1][0].cnt > 5) {
-                            this.updateGetMoney(company_Idx);
-                        } else {
-                            resolve(res[1][0].cnt);
+                        if(parseInt(res[1][0].cnt) >= 0){
+                            dbconn.query(`update company_info set company_stock = concat(company_stock + 500) where company_idx = '${company_Idx}';`,(err,result,fields)=>
+                            {
+                                if(err){
+                                    console.log(err);
+                                    reject(err);
+                                } else{
+                                    resolve(10);
+                                }
+                            });
+                        }
+                       
+                    }
+                })
+        })
+    },
+
+    sellStock : function(stock, user_idx, company_Idx) {
+        return new Promise ((resolve, reject)=>{
+            var query = `insert into ${table2} (get_sell_stock, user_idx, company_Idx) values ('${stock}','${user_idx}','${company_Idx}');`+
+                        `select count(*) as cnt from stock_sell_item b
+                        inner join company_info c on b.company_idx = c.company_idx
+                        where b.get_sell_stock = c.company_stock`
+            dbconn.query(query, (err,result,fields) =>
+                {
+                    if(err){
+                        console.log(err);
+                        reject(err);
+                    } else{
+                        let res = JSON.parse(JSON.stringify(result));
+                        if(parseInt(res[1][0].cnt) >= 0){
+                            dbconn.query(`update company_info set company_stock = concat(company_stock - 500) where company_idx = '${company_Idx}';`,(err,result,fields)=>
+                            {
+                                if(err){
+                                    console.log(err);
+                                    reject(err);
+                                } else{
+                                    resolve(10);
+                                }
+                            });
                         }
                     }
                 }
@@ -31,35 +66,4 @@ module.exports = {
         })
     },
 
-    sellStock : function(stock, user_idx, company_Idx) {
-        return new Promise ((resolve, reject)=>{
-            dbconn.query(
-                `insert into ${table2} (get_sell_stock, user_idx, company_Idx) values ('${stock}','${user_idx}','${company_Idx}')`, (err,result,fields) =>
-                {
-                    if(err){
-                        console.log(err);
-                        reject(err);
-                    } else{
-                        resolve(result);
-                    }
-                }
-            )
-        })
-    },
-
-    updateGetMoney : function(company_Idx){
-        return new Promise ((resolve, reject) =>{
-            dbcoon.query(
-                `update company_info set company_stock = concat(company_stock + 500) where company_idx = '${company_Idx}';`, (err,result,fields) =>
-                {
-                    if(err){
-                        console.log(err);
-                        reject(err);
-                    } else{
-                        resolve(result);
-                    }
-                }
-            )
-        })
-    }
 }
