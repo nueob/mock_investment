f6918ebe5b9d9e8b5e02f5f7462645e080dddeb5
hyeonung <ntweety@naver.com>
2021-12-02 02:00:49 +0900

    cheerio img try

--- mock_investment/index1.js
+++ mock_investment/index1.js
@@ -1,6 +1,14 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const Iconv=require('iconv-lite');
+const fs = require('fs');
+
+fs.readdir('poster', (err) => {
+  if(err){
+    console.error('poster 폴더가 없어 poster 폴더를 생성합니다.');
+    fs.mkdirSync('poster');
+  }
+})
  
 const getHtml = async (keyword) => {
     try {
@@ -20,15 +28,24 @@ const getData = async(keyword) => {
     const $bodyList = $("p.no_today");
     let titles = [];
     
+    var img = $('#img_chart_area').attr('src');
+    if (img){
+      const imgResult = await axios.get(img, {
+        responseType: 'arraybuffer',
+      });
+      console.log(imgResult);
+      fs.writeFileSync(`poster/aa`, imgResult.data);
+    }
+    console.log(img);
 
     $bodyList.each((idx, elem)=> {
        
       titles=String($(elem).find('span:nth-of-type(1)').text().trim());
       titles=parseInt(titles.replace(',',''));
-      console.log(titles);
+      // console.log(titles);
      
       
-      
+     
     });
  
     
